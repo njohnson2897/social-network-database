8ac05ad5d58e806ad77f9ee74a834a58d3e0b019
Nate Johnson <njohnson2897@gmail.com>
2024-04-24 09:39:04 -0500

    adds all unpopulated thoughts and reactions routes

---
+++ controllers/thoughtController.js
@@ -0,0 +1,32 @@
+const { ObjectId } = require('mongoose').Types;
+const { Thought, User } = require('../models');
+
+module.exports = {
+    async getThoughts(req, res) {
+
+    },
+
+    async getSingleThought(req, res) {
+
+    },
+
+    async addThought(req, res) {
+
+    },
+
+    async removeThought(req, res) {
+
+    },
+
+    async updateThought(req, res) {
+
+    },
+
+    async addReaction(req, res) {
+
+    },
+
+    async removeReaction(req, res) {
+
+    },
+}
