62303836bf940bb6b3f007b6b393cabda4fa07fa
Nate Johnson <njohnson2897@gmail.com>
2024-04-24 12:17:26 -0500

    adds add thought, remove thought, and update thought routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,6 +2,7 @@ const { ObjectId } = require('mongoose').Types;
 const { Thought, User } = require('../models');
 
 module.exports = {
+    // GET all thoughts
     async getThoughts(req, res) {
         try {
             const thoughts = await Thought.find();
@@ -11,7 +12,7 @@ module.exports = {
             return res.status(500).json(err)
         }
     },
-
+    // GET thought by ID
     async getSingleThought(req, res) {
         try {
             const thought = await Thought.findOne({ _id: req.params.thoughtId });
@@ -26,17 +27,46 @@ module.exports = {
             return res.status(500).json(err);
         }
     },
-
+    // create thought - req.body should look like this: { "thoughtText": ".......", "username": "......."}
     async addThought(req, res) {
-
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(thought);
+        }  catch (err) {
+            res.status(500).json(err);
+        }
     },
-
+    // DELETE thought by ID
     async removeThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndRemove({ _id: req.params.thoughtId });
 
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID'});
+            }
+
+            res.json({ message: 'Thought removed'});
+        } catch (err) {
+            res.status(500).json(err);
+        }
     },
-
+    // update thought by ID - req.body should look like this: { "thoughtText": "....", "username": "username"}
     async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                // run validators?
+            );
 
+            if (!thought) {
+                res.status(404).json({ message: 'No thought with that ID'});
+            }
+
+            res.json(thought);
+        } catch (err)  {
+            res.status(500).json(err);
+        }
     },
 
     async addReaction(req, res) {
