96a8bf1d3a2ed4fcba3add52f858c33b054b30a0
Nate Johnson <njohnson2897@gmail.com>
2024-04-24 09:59:46 -0500

    debugs mongoose match validator so that users can be added

--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const userSchema  = new Schema(
             type: String,
             required: true,
             unique: true,
-            match: '/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/'
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address']
         },
         thoughts: [
             {
