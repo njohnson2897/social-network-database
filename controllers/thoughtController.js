81a7ae63f54346940ac85ce81ad32830ab6d1b40
Nate Johnson <njohnson2897@gmail.com>
2024-04-24 12:12:19 -0500

    adds get thought and get thought by ID routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,11 +3,28 @@ const { Thought, User } = require('../models');
 
 module.exports = {
     async getThoughts(req, res) {
-
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err)
+        }
     },
 
     async getSingleThought(req, res) {
-
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId });
+    
+            if (!thought) {
+                return res.status(404).json({message: 'No thought with that ID' })
+            }
+    
+            res.json(thought);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
     },
 
     async addThought(req, res) {
